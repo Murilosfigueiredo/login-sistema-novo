25f1004d8edc4d508fe235488a0ae679d857d0c9
Murilosfigueiredo <murilofigueiredo415263@gmail.com>
2026-03-24 19:20:42 -0300

    Implement basic login functionality

---
+++ Login.java
@@ -0,0 +1,17 @@
+public class Login  {
+  public static void main(String[]
+                          args)  {
+            // O SE (se a senha for 1234)
+        if (senhaCorreta == 1234) {
+            System.out.println("Acesso Liberado");
+        } 
+        
+        // O SENÃO (se errar a senha)
+        else {
+            System.out.println("Acesso Negado");
+        }
+        
+    } // Fecha o Motor
+} // Fecha a Casa
+
+      
